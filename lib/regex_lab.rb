8ab367370847eda7a40c39aa7db4545198ba9762
Hanna Lee <mani21na@gmail.com>
2019-08-19 21:41:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   #!!text.match(/^[A-Z]\w+[[:punct:]]/)
   #!!text.match(/^[A-Z].+\W$/)
-  !!text.match(/\^[A-Z].+[.!?]/)
+  !!text.match(/^[A-Z].+[.!?]/)
 end
 
 def valid_phone_number?(phone)
